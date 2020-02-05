1bc02544ddf450bc987fcbc28531f108f4c102a8
Eric Zhang <ericweikaizhang@gmail.com>
2020-02-05 14:04:56 -0800

    lab4 adding local storage and filter by keyword

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+var artists=[];
+
+window.onload = function(){
+    loadLocalStorage()
+}
+
+class Artist {
+    constructor(artistName, aboutArtist, imgUrl, li){
+        this.artistName = artistName;
+        this.aboutArtist = aboutArtist;
+        this.imgUrl = imgUrl;
+        this.li = li;
+    }
+} 
+
 function toggleForm() {
     let form = document.getElementById("add_artist_form");
     if (form.style.display === "none") {
@@ -8,7 +23,35 @@ function toggleForm() {
     form.reset();
 }
 
-function appendArtist() {
+function appendArtist(artist){
+    let li = document.createElement("li");
+    let div1 = document.createElement("div");
+    let div2 = document.createElement("div");
+    let img = document.createElement("img");
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+    let btn = document.createElement("button");
+    div1.className = "img_col";
+    div2.className = "info_col";
+    p1.className = "artists_names";
+    p2.className = "artists_institutions";
+    btn.className = "del_btn_col";
+    btn.onclick = function(){removeArtist(artist,btn)};
+    artist.li = li;
+    img.src = artist.imgUrl;
+    p1.textContent = artist.artistName;
+    p2.textContent = artist.aboutArtist;
+    btn.textContent = "Delete";
+    div1.appendChild(img);
+    div2.appendChild(p1);
+    div2.appendChild(p2);
+    li.appendChild(div1);
+    li.appendChild(div2);
+    li.appendChild(btn);
+    document.getElementById("artists_container").firstElementChild.appendChild(li);
+}
+
+function submitAddForm() {
     if(validateForm()==1)
         return;
     let form = document.getElementById("add_artist_form");
@@ -24,38 +67,42 @@ function appendArtist() {
         if(input.name=="img_url")
             imgUrl = input.value;
     }
+
     console.log(artistName);
     console.log(aboutArtist);
     console.log(imgUrl);
-    let li = document.createElement("li");
-    let div1 = document.createElement("div");
-    let div2 = document.createElement("div");
-    let img = document.createElement("img");
-    let p1 = document.createElement("p");
-    let p2 = document.createElement("p");
-    let btn = document.createElement("button");
-    div1.className = "img_col";
-    div2.className = "info_col";
-    p1.className = "artists_names";
-    p2.className = "artists_institutions";
-    btn.className = "del_btn_col";
-    btn.onclick = function(){removeArtist(btn)};
-    img.src = imgUrl;
-    p1.textContent = artistName;
-    p2.textContent = aboutArtist;
-    btn.textContent = "Delete";
-    div1.appendChild(img);
-    div2.appendChild(p1);
-    div2.appendChild(p2);
-    li.appendChild(div1);
-    li.appendChild(div2);
-    li.appendChild(btn);
-    document.getElementById("artists_container").firstElementChild.appendChild(li);
+    let artist = new Artist(artistName, aboutArtist, imgUrl, null);
+    appendArtist(artist);
+    artists.push(artist);
+    localStorage.setItem("artists", JSON.stringify(artists));
     toggleForm();
+    showAllArtists();
 }
 
-function removeArtist(btn){
+function removeArtist(artist,btn){
     btn.parentNode.remove();
+    const index = artists.indexOf(artist);
+    if (index > -1)
+        artists.splice(index, 1);
+    localStorage.setItem("artists", JSON.stringify(artists));
+}
+
+function filterArtists(keyWord){
+    if(keyWord==null)
+        keyWord = document.getElementById("search_field").value;
+    artists.forEach(function(artist){
+        if(!artist.artistName.toUpperCase().includes(keyWord.toUpperCase())){
+            artist.li.style.display = "none";
+        }
+        else{
+            artist.li.style.display = "block";
+        }
+    })
+    document.getElementById("search_field").value=keyWord;
+}
+
+function showAllArtists(){
+    filterArtists("");
 }
 
 function validateForm() {
@@ -74,4 +121,15 @@ function validateForm() {
         }
     }
     return 0;
-  }
+}
+
+function loadLocalStorage(){
+    artists = JSON.parse(localStorage.getItem("artists"));
+    if(artists===null)
+        artists = [];
+    else{
+        artists.forEach(function(artist){
+            appendArtist(artist);
+        })
+    }
+}
